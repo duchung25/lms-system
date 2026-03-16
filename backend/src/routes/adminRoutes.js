73f7cda82d81475d9f12c354e8948984b712029b
DucHung <hungbnjkl@gmail.com>
2026-03-16 22:25:07 +0700

    delete trash and commit env.example

--- backend/src/routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import adminController from "../controllers/adminController.js";
-
-const router = express.Router();
-router.get('/users', adminController.getAllUsers);
-
-export default router;
