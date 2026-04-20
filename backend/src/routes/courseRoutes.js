094237977adab357ab9963f398fd1e7c0b5ad1a8
DucHung <hungbnjkl@gmail.com>
2026-04-21 00:07:30 +0700

    done createLesson and display it

--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -3,8 +3,11 @@ import courseController from '../controllers/courseController.js';
 import courseValidator from '../validators/courseValidator.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import writeAccessMiddleware from '../middlewares/writeAccessMiddleware.js';
+import lessonRoutes from './lessonRoutes.js';
 
 const router = express.Router();
+
+router.use('/', lessonRoutes);
 
 router.get('/by-teacher/:teacherId', authMiddleware, courseController.getCoursesByTeacher);
 router.get('/my-courses', authMiddleware, courseController.getMyCourses);
@@ -13,10 +16,10 @@ router.patch('/:courseId/unpublish', authMiddleware, writeAccessMiddleware, cour
 
 router.post('/', authMiddleware, courseValidator.createCourseValidationRules, courseValidator.validate, courseController.createCourse);
 router.get('/', authMiddleware,  courseController.getAllCourse);
+router.get('/count/summary', authMiddleware, courseController.countHandler);
 router.get('/:courseId', authMiddleware, courseController.getCourseDetail);
 router.patch('/:courseId', authMiddleware, writeAccessMiddleware, courseValidator.updateCourseValidationRules, courseValidator.validate, courseController.updateCourse);
 router.delete('/:courseId', authMiddleware, writeAccessMiddleware, courseController.deleteCourse);
 router.patch('/:courseId/restore', authMiddleware, writeAccessMiddleware, courseController.restoreCourse);
-router.get('/count/summary', authMiddleware, courseController.countHandler);
 
 export default router;
